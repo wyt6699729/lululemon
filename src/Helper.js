821c31928c21f8442c409b243d97c0cae36af402
wyt6699729 <yitianwang1991@gmail.com>
2022-07-29 14:29:23 -0700

    Created Collection list component

---
+++ src/Helper.js
@@ -0,0 +1,7 @@
+export const collection = {
+    collectionTitle: "Collection",
+    collectionList: [
+        "Align", "Base Pace", "Cool Racerback", "Dance Studio", "Define", "Ebb To Street",
+        "Energy", "Free To Be", "Love", "Swiftly", "Wunder Train"
+    ]
+}
